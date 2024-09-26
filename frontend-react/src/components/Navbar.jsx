9813b59dcfdac4b0e9f38c35fd9cf60f6ccea8f3
Aj242005 <akshitjain6661@gmail.com>
2024-09-27 01:03:51 +0530

    Adding element to Navbar

--- frontend-react/src/components/Navbar.jsx
+++ frontend-react/src/components/Navbar.jsx
@@ -63,7 +63,7 @@ function Navbar(){
 
 
 
-                <form className="d-flex" role="search">
+                <form className="d-flex" role="search" style={{marginLeft:"400px",marginRight:"-120px"}}>
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                     <button className="btn btn-outline-success" type="submit">Search</button>
                 </form>
