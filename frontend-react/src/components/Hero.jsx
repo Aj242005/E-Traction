87310ed0f295d2c415cffaab5c4810187acdd022
Aj242005 <akshitjain6661@gmail.com>
2024-09-28 00:33:58 +0530

    Adding element to Navbar

--- frontend-react/src/components/Hero.jsx
+++ frontend-react/src/components/Hero.jsx
@@ -5,7 +5,6 @@ function Hero() {
     return <div className="apna-h-hero">
         <div className="apna-h-hero-text">
             <h1>The X Factor of EV-Infra</h1>
-
         </div>
     </div>
 }
