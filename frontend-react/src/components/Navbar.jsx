261766a62a8da51996f058c188e7dd66cf053f2d
Aj242005 <akshitjain6661@gmail.com>
2024-09-28 02:30:54 +0530

    Sign up/login

--- frontend-react/src/components/Navbar.jsx
+++ frontend-react/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
         <nav className="navbar bg-body-tertiary apna-navbar">
             <div className="container-fluid ">
                 <a className="navbar-brand fs-2  apna-main-logo text-info" href="/" target="" style={{margin: "10px", marginLeft: "3rem", fontFamily: "Comic Sans MS,cursive"}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="52" height="84.5" fill="currentColor" className="bi bi-lightning-charge-fill apna-logo" viewBox="2 2 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="60" fill="currentColor" className="bi bi-lightning-charge-fill apna-logo" viewBox="2 2 16 16">
                         <path
                             d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
                     </svg>
@@ -17,10 +17,6 @@ function Navbar(){
                     </svg>
                 </sup>
                 </a>
-
-
-
-
 
 
 
--- frontend-react/src/landing_pages/Sign_up.jsx
+++ frontend-react/src/landing_pages/Sign_up.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './Sign_up.css'
 
+
+import user_icon from '../assets/person.png';
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/password.png';
+
+
 function SignUp() {
+
     return <div className="apna-s-container">
         <div className="apna-s-header">
             <div className="apna-s-text">
@@ -11,8 +18,22 @@ function SignUp() {
         </div>
         <div className="apna-s-inputs">
             <div className="apna-s-input">
-                
+                <img src={user_icon} alt="user icon" />
+                <input type="text" placeholder="User Name....." />
             </div>
+            <div className="apna-s-input">
+                <img src={email_icon} alt="email icon" />
+                <input type="email" placeholder="Email....." />
+            </div>
+            <div className="apna-s-input">
+                <img src={password_icon} alt="password icon" />
+                <input type="password" placeholder="Password....." />
+            </div>
+        </div>
+        <div className="apna-s-forgot-password">Lost Password ? <span>Click Here!</span></div>
+        <div className="apna-s-submit-container">
+            <div className="apna-s-submit">Sign Up</div>
+            <div className="apna-s-submit">Log In</div>
         </div>
     </div>
 }
