6e5eccbda0152a9a37ace11d6061e2e8e77afb92
Aj242005 <akshitjain6661@gmail.com>
2024-09-28 12:39:07 +0530

    CI/CD pipleline

--- frontend-react/src/landing_pages/About.jsx
+++ frontend-react/src/landing_pages/About.jsx
@@ -5,9 +5,9 @@ import next_icon from '../assets/next-icon.png'
 import back_icon from '../assets/back-icon.png'
 import member1 from '../assets/team_members/member1.jpeg'
 import member2 from '../assets/team_members/member2.jpeg'
-import member3 from '../assets/team_members/member3.jpeg'
-import member4 from '../assets/team_members/member4.jpeg'
 import member5 from '../assets/team_members/member5.jpeg'
+// import member4 from '../assets/team_members/member4.jpeg'
+// import member5 from '../assets/team_members/member5.jpeg'
 
 function About() {
     return <>
